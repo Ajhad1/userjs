85a2b338307245a0ad908c597360be30c9732dac
Alejandro <alejandro@alejandroc.com>
2020-08-27 09:51:10 -0400

    Add border color changing functionality
    Allows the ability to change border colors to match the background color set

--- NoBrighter.user.js
+++ NoBrighter.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/henix/userjs/NoBrighter
 // @description Change element's background color that is too bright to a light green.
 // @author      henix
-// @version     20200213.1
+// @version     20160608.1
 // @include     http://*
 // @include     https://*
 // @exclude     http://boards.4chan.org/*
@@ -55,7 +55,9 @@
 // var targetColor = '#C7EDCC'; // 93
 // var targetColor = '#C1E6C6'; // 90
 
-var targetColor = '#EEEEEE';
+/* Gray */
+var targetColor = '#dddddd'; // 90
+var newBorderColor = '#ffffff'; // 90
 
 /* Wheat */
 // var targetColor = '#E6D6B8'; // 90
@@ -69,6 +71,7 @@ var longRunSites = [
   'docs.google.com',
   'plus.google.com',
   'groups.google.com',
+    'calendar.google.com',
 
   'twitter.com',
   'github.com',
@@ -82,77 +85,218 @@ var longRunSites = [
 
   'feedly.com',
   'reader.aol.com',
+  'identityfusion.atlassian.net',
 ];
 
 var $minHeight = 6;
 
 // ========== End of config ========== //
 
+var alltags = document.getElementsByTagName("*"); // query all elements to change background
+var bodyChanged = false; // for tracking changed elements
+
+// check if background is transparent
 function isTransparent(color) {
     return color === 'transparent' || color.replace(/ /g, '') === 'rgba(0,0,0,0)';
 }
 
+// change background to set color
 function changeBgcolor(elem) {
-  if (elem.nodeType !== Node.ELEMENT_NODE) {
-    return;
-  }
-  var bgcolor = window.getComputedStyle(elem, null).backgroundColor;
-  if (bgcolor && !isTransparent(bgcolor) && elem.clientHeight >= $minHeight) {
-    var arRGB = bgcolor.match(/\d+/g);
-    var r = parseInt(arRGB[0], 10);
-    var g = parseInt(arRGB[1], 10);
-    var b = parseInt(arRGB[2], 10);
-
-    // we adopt HSL's lightness definition, see http://en.wikipedia.org/wiki/HSL_and_HSV
-    var brightness = (Math.max(r, g, b) + Math.min(r, g, b)) / 255 / 2;
-
-    if (brightness > Brightness_Threshold) {
-      elem.style.backgroundColor = targetColor;
+    // check if appropriate type of element
+    if (elem.nodeType !== Node.ELEMENT_NODE) {
+        return;
     }
-    return true;
-  } else {
-    return false;
-  }
+
+    // compute effective background color of element
+    var bgcolor = window.getComputedStyle(elem, null).backgroundColor;
+
+    // if background is not transparent and within set height change the background color
+    if (bgcolor && !isTransparent(bgcolor) && elem.clientHeight >= $minHeight) {
+        var arRGB = bgcolor.match(/\d+/g);
+        var r = parseInt(arRGB[0], 10);
+        var g = parseInt(arRGB[1], 10);
+        var b = parseInt(arRGB[2], 10);
+
+        // we adopt HSL's lightness definition, see http://en.wikipedia.org/wiki/HSL_and_HSV
+        var brightness = (Math.max(r, g, b) + Math.min(r, g, b)) / 255 / 2;
+
+        // compare background brightness with threshold set
+        if (brightness > Brightness_Threshold) {
+            // change background color if threshold was exceeded
+            elem.style.backgroundColor = targetColor;
+        }
+        return true;
+    } else {
+        return false;
+    }
 }
 
+// unknown function at this time
 function changeTransparent(elem) {
-  var bgcolor = window.getComputedStyle(elem, null).backgroundColor;
-  if (!bgcolor || isTransparent(bgcolor)) {
-    elem.style.backgroundColor = targetColor;
-  }
+    // computer background color of element
+    var bgcolor = window.getComputedStyle(elem, null).backgroundColor;
+
+    // if background color does not match or is transparent
+    if (!bgcolor || isTransparent(bgcolor)) {
+        // change background color to set color
+        elem.style.backgroundColor = targetColor;
+    }
 }
-
-var alltags = document.getElementsByTagName("*");
-
-var bodyChanged = false;
 
 function changeAll() {
-  var len = alltags.length;
-  for (var i = 0; i < len; i++) {
-    var changed = changeBgcolor(alltags[i]);
-    var tagName = alltags[i].tagName.toUpperCase();
-    if (changed && (tagName === "BODY" || tagName === "HTML")) {
-      bodyChanged = true;
+    // iterate through all elements on site
+    var len = alltags.length;
+    for (var i = 0; i < len; i++) {
+        // change background colors and track changed elements
+        var changed = changeBgcolor(alltags[i]);
+
+        // format elements names for tracking
+        var tagName = alltags[i].tagName.toUpperCase();
+
+        // if element changed was BODY or HTML mark bodyChanged as true
+        if (changed && (tagName === "BODY" || tagName === "HTML")) {
+            bodyChanged = true;
+        }
     }
-  }
 }
+
+// creates css to adjust pseudo after properties
+var newStyleSheet = document.createElement('style');
+newStyleSheet.innerHTML = `
+    /* changes border color for pseudo after elements */
+    .changeBorder:after {
+        border-color: `+newBorderColor+`;
+    }
+    /* changes border colors on class Box */
+    .Box, .Box-header {
+        border-color: `+newBorderColor+`;
+    }
+	`;
+// append new style sheet to website
+window.document.head.appendChild(newStyleSheet);
+
+// get all children of children
+function getDescendants(node, accum) {
+    var i;
+    accum = accum || [];
+    for (i = 0; i < node.childNodes.length; i++) {
+        accum.push(node.childNodes[i])
+        getDescendants(node.childNodes[i], accum);
+    }
+    return accum;
+}
+
+// change border color
+function changeBorderColor(elem) {
+    // confirm appropriate type of element
+    if (elem.nodeType !== Node.ELEMENT_NODE) {
+        return;
+    }
+    // computer bordercolor of element
+    var borderColor = window.getComputedStyle(elem, null).borderColor;
+
+    // checks if element has a border color and if it is transparent or not
+    if (borderColor && !isTransparent(borderColor)) {
+        var arRGB = borderColor.match(/\d+/g);
+        var r = parseInt(arRGB[0], 10);
+        var g = parseInt(arRGB[1], 10);
+        var b = parseInt(arRGB[2], 10);
+
+        // we adopt HSL's lightness definition, see http://en.wikipedia.org/wiki/HSL_and_HSV
+        var brightness = (Math.max(r, g, b) + Math.min(r, g, b)) / 255 / 2;
+
+        // computes whether element has pseudo :after css
+        var pseudoVar = window.getComputedStyle(elem, ":after").borderColor;
+
+        // currently debugging brightness levels
+        if (brightness > .5) {
+            // changes element border to newBorderColor
+            elem.style.borderColor = newBorderColor;
+
+            // if element has pseudo :after css adds preset css class to override it
+            if (pseudoVar) {
+                // adds class to element
+                elem.classList.add("changeBorder");
+            }
+            // console.log(elem.nodeName + " brightness is greater than 5")
+        } else {
+            // changes element border to newBorderColor
+            elem.style.borderColor = newBorderColor;
+
+            // if element has pseudo :after css adds preset css class to override it
+            if (pseudoVar) {
+                // adds class to element
+                elem.classList.add("changeBorder");
+            }
+            // console.log(elem.nodeName + " brightness is less than 5")
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// iterate through all divs in grid to change border colors
+function changeAllBorder() {
+    // choose elements to target for change border
+    var allDivs = document.querySelectorAll('div[role="grid"]'); // example: var allDivs = document.querySelectorAll('div[role="grid"],div[role="gridcell"],div[role="row"]');
+
+    // get all matching divs and iterate through them
+    var len = allDivs.length;
+    for (var i = 0; i < len; i++) {
+        // changes all top elements
+        changeBorderColor(allDivs[i]);
+
+        // calculates all descendants
+        var allDivsDescendants = getDescendants(allDivs[i]);
+
+        // iterates through all descendants
+        var len2 = allDivsDescendants.length;
+        for (var i2 = 0; i2 < len2; i2++) {
+            // changes border of all descendants
+            changeBorderColor(allDivsDescendants[i2]);
+
+            // checks if descendants have children and changes those as well
+            if (allDivsDescendants.hasChildren) {
+                // calculates all children of descendants
+                var allDivsDescendantsChildren = allDivsDescendants.children;
+
+                // iterates through all children of descendants
+                var len3 = allDivsDescendantsChildren.length;
+                for (var i3 = 0; i3 < len3; i3++) {
+                    // changes border of all children of descendants
+                    changeBorderColor(allDivsDescendantsChildren[i3]);
+                }
+            }
+        }
+    }
+}
+
 changeAll();
+changeAllBorder();
 
 if (window.top == window) {
-  // change transparent only when in top frame
-  if (!bodyChanged) {
-    changeTransparent(document.body.parentNode);
-  }
+    // change transparent only when in top frame
+    if (!bodyChanged) {
+        changeTransparent(document.body.parentNode);
+    }
 }
 
+// iterates through all sites set in longRun section
 for (var i = 0; i < longRunSites.length; i++) {
-  if (location.hostname === longRunSites[i]) {
-    console.info('make NoBrighter runs forever...');
-    setInterval(changeAll, 2000); // convert every 2s
-    break;
-  }
+    if (location.hostname === longRunSites[i]) {
+        // logs in console if site is going to be checked forever
+        console.info('make NoBrighter runs forever...');
+
+        // sets 2 second interval to run functions
+        setInterval(changeAll, 2000); // convert every 2s
+        setInterval(changeAllBorder, 2000); // convert every 2s
+        break;
+    }
 }
 
-/*document.body.addEventListener('DOMNodeInserted', function(e) {
+/*
+document.body.addEventListener('DOMNodeInserted', function(e) {
   changeBgcolor(e.target);
-}, false);*/
+}, false);
+*/
